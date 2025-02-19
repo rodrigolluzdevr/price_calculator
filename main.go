8172ac6b9a580d94973c57b16377698964e2b1d5
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-02-19 15:17:35 -0300

    feat: prices.go add

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"price-calculator/prices"
+)
 
+func main() {
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	for _, taxRates := range taxRates {
+		priceJob := prices.NewTaxIncludedPriceJob(taxRates)
+		priceJob.Process()
+	}
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,7 +1,27 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+
+	fmt.Println(result)
+
+}
+
+func NewTaxIncludedPriceJob(TaxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{100, 200, 300},
+		TaxRate:     TaxRate,
+	}
 }
